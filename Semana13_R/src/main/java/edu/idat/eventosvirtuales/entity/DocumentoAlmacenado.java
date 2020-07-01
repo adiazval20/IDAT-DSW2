7b2d8a55b29bbcd8760a715771f820a1d3a09509
Andy <adiazval20@gmail.com>
2020-06-30 20:19:22 -0500

    1

---
+++ Semana13_R/src/main/java/edu/idat/eventosvirtuales/entity/DocumentoAlmacenado.java
@@ -0,0 +1,13 @@
+package edu.idat.eventosvirtuales.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class DocumentoAlmacenado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}
